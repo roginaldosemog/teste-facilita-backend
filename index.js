e9d39b91ba1506fadbfa232a9280a33dfeeeb74e
Igor Gomes <roginaldosemog@gmail.com>
2024-01-30 00:43:35 -0300

    feat: add express and dotenv to project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello World")
+require("dotenv").config();
+
+const port = process.env.PORT;
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "ok!"
+    })
+})
+
+app.listen(port);
+
+console.log(`Server running at port ${port}`);
